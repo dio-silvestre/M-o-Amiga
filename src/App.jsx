2662cde3ce8a8b8547b33b1eb21f0b877c683e20
Diogo Nava <diogonsdf@gmail.com>
2021-09-08 17:42:23 -0300

    fix: tests deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,11 @@
-import Input from "./components/Input";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
-import { FiMail, FiUser } from "react-icons/fi";
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 
 function App() {
-  const schema = yup.object().shape({
-    username: yup.string().required("Nome de usuário obrigatório"),
-    password: yup.string().required("Senha obrigatória"),
-  });
-
-  const {
-    register,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
   return (
     <>
       <GlobalStyle />
       <Routes />
-      <Input
-        register={register}
-        name="username"
-        icon={FiUser}
-        label="Nome de usuário"
-        placeholder="Nome de usuário"
-        error={errors.username?.message}
-        colorSchema
-      />
-      <Input
-        register={register}
-        name="password"
-        icon={FiMail}
-        label="Senha"
-        placeholder="Sua senha"
-        type="password"
-        error={errors.password?.message}
-      />
     </>
   );
 }
