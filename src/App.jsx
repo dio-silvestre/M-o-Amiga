7a8c2ce977fce113992af459ee84aac52e601704
Diogo Nava <diogonsdf@gmail.com>
2021-09-08 20:30:40 -0300

    feat: hot-toast lib added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Login from "./pages/Login";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/global";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+      <Toaster position="top-center" reverseOrder={false} />
       <GlobalStyle />
       <Routes />
       <Login />
