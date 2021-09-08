3ba8cf9b00bfd34c107dcdb82c650f21bddb8732
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-08 18:05:52 -0300

    formSchema added

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,6 +2,18 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
-function Form() {}
+function Form() {
+  const formSchema = yup.object().shape({
+    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+    name: yup.string().required("Nome obrigatório"),
+    city: yup.string().required("Cidade obrigatória"),
+    state: yup.string().required("Estado obrigatório"),
+    interestField: yup.string().required("Endereço obrigatório"),
+    password: yup.string().required("Senha obrigatória"),
+    password2: yup.string().required("Senhas não correspondentes"),
+  });
+
+  return()
+}
 
 export default Form;
