93a672a20ad1172c84313e4761eaf38788d335ce
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-09 16:30:46 -0300

    style:inital commit from register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -13,6 +13,16 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Msg1,
+  Msg2,
+  H3,
+  Blue,
+  AnimationContainer,
+  Container,
+  Form,
+  Ul,
+} from "./styles";
 
 const Register = () => {
   const formSchema = yup.object().shape({
@@ -36,104 +46,127 @@ const Register = () => {
   const onSubmitFunction = (data) => console.log(data);
 
   return (
-    <div className="container">
-      <h3>Crie sua conta</h3>
-      <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
-        <ul className="TypeChoice">
-          <li>
-            <input
-              type="radio"
-              value="instituition"
-              name="radio"
-              id="instituition"
-            />
-            <label for="instituition">Instituição</label>
-          </li>
-          <li>
-            <input type="radio" value="voluntary" name="radio" id="voluntary" />
-            <label for="voluntary">Voluntário</label>
-          </li>
-        </ul>
+    <>
+      <Blue>
+        <Msg1>
+          <h2>Recrute!</h2>
+          <h3>Aqui você pode recrutar voluntários para sua ação social</h3>
+        </Msg1>
+        <Msg2>
+          <h2>Procurando fazer o bem?</h2>
+          <h3>
+            Aqui você pode pesquisar por eventos sociais para fazer parte!
+          </h3>
+        </Msg2>
+      </Blue>
+      <H3>Crie sua conta</H3>
+      <AnimationContainer>
+        <Container>
+          <Form>
+            <form className="form" onSubmit={handleSubmit(onSubmitFunction)}>
+              <Ul>
+                <li>
+                  <input
+                    type="radio"
+                    value="instituition"
+                    name="radio"
+                    id="instituition"
+                  />
+                  <label for="instituition">Instituição</label>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    value="voluntary"
+                    name="radio"
+                    id="voluntary"
+                  />
+                  <label for="voluntary">Voluntário</label>
+                </li>
+              </Ul>
 
-        <Input
-          register={register}
-          name="name"
-          icon={FiUser}
-          label="Nome"
-          placeholder="Digite seu nome"
-          colorSchema
-          error={errors.name?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="name"
+                icon={FiUser}
+                label="Nome"
+                placeholder="Digite seu nome"
+                colorSchema
+                error={errors.name?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="email"
-          icon={FiMail}
-          label="E-mail"
-          placeholder="Digite seu e-mail"
-          colorSchema
-          error={errors.email?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="email"
+                icon={FiMail}
+                label="E-mail"
+                placeholder="Digite seu e-mail"
+                colorSchema
+                error={errors.email?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="city"
-          icon={FiMapPin}
-          label="Cidade"
-          placeholder="Informe sua cidade"
-          colorSchema
-          error={errors.city?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="city"
+                icon={FiMapPin}
+                label="Cidade"
+                placeholder="Informe sua cidade"
+                colorSchema
+                error={errors.city?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="state"
-          icon={FiMap}
-          label="Estado"
-          placeholder="Informe seu estado"
-          colorSchema
-          error={errors.state?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="state"
+                icon={FiMap}
+                label="Estado"
+                placeholder="Informe seu estado"
+                colorSchema
+                error={errors.state?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="areas_interest"
-          icon={FiHeart}
-          label="Áreas de interesse"
-          placeholder="Informe suas áreas de interesse"
-          colorSchema
-          error={errors.areas_interest?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="areas_interest"
+                icon={FiHeart}
+                label="Áreas de interesse"
+                placeholder="Informe suas áreas de interesse"
+                colorSchema
+                error={errors.areas_interest?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="password"
-          icon={FiKey}
-          label="Senha"
-          placeholder="Informe sua senha"
-          colorSchema
-          error={errors.password?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="password"
+                icon={FiKey}
+                label="Senha"
+                placeholder="Informe sua senha"
+                colorSchema
+                error={errors.password?.message}
+              ></Input>
 
-        <Input
-          register={register}
-          name="password2"
-          icon={FiLock}
-          label="Confirmação de Senha"
-          placeholder="Confirme a senha"
-          colorSchema
-          error={errors.password2?.message}
-        ></Input>
+              <Input
+                register={register}
+                name="password2"
+                icon={FiLock}
+                label="Confirmação de Senha"
+                placeholder="Confirme a senha"
+                colorSchema
+                error={errors.password2?.message}
+              ></Input>
 
-        <Button type="submit" theme="register">
-          Finalizar Cadastro
-        </Button>
+              <Button type="submit" theme="register">
+                Finalizar Cadastro
+              </Button>
 
-        <p>
-          Já possui uma conta? <Link to="/login">LOGIN</Link>
-        </p>
-      </form>
-    </div>
+              <p>
+                Já possui uma conta? <Link to="/login">LOGIN</Link>
+              </p>
+            </form>
+          </Form>
+        </Container>
+      </AnimationContainer>
+    </>
   );
 };
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,1 +1,172 @@
 import styled, { keyframes } from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  font-family: var(--font-text);
+`;
+
+//
+
+export const Blue = styled.div`
+  width: 100vw;
+  height: 449px;
+  background-color: var(--color-primary);
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+`;
+
+export const Msg1 = styled.div`
+  font-family: var(--font-text);
+  color: white;
+  line-height: 32px;
+  font-weight: bold;
+  h2 {
+    font-size: 26px;
+  }
+  h3 {
+    font-size: 20px;
+  }
+
+  position: absolute;
+  width: 333.29px;
+  height: 89.68px;
+  left: 601px;
+  top: 69px;
+`;
+export const Msg2 = styled.div`
+  font-family: var(--font-text);
+  color: white;
+  line-height: 32px;
+  font-weight: bold;
+  h2 {
+    font-size: 26px;
+  }
+  h3 {
+    font-size: 20px;
+  }
+
+  position: absolute;
+  width: 333.29px;
+  height: 119.13px;
+  left: 1000.39px;
+  top: 260.4px;
+`;
+
+export const H3 = styled.h3`
+  color: var(--color-text);
+`;
+
+export const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 200%;
+  height: 80%;
+  border: 2px solid var(--color-primary-two);
+  border-radius: var(--border-radius);
+  background-color: var(--color-base-default);
+
+  position: absolute;
+  width: 420px;
+  height: 710px;
+  left: 291px;
+  top: 282px;
+
+  button {
+    background-color: var(--color-secondary-two);
+  }
+  button:hover {
+    background-color: var(--color-secondary);
+  }
+
+  input {
+    max-width: 150%;
+  }
+`;
+
+export const Ul = styled.ul`
+width:100%;
+
+  li {
+    display: inline-block;
+    color: var(--color-text-two);
+    padding-bottom: 10px;     
+    
+    input {
+    visibility: hidden;
+    /* display: flex;
+    flex-wrap: nowrap; */
+  }
+  label {
+    
+
+    width:100%;
+    cursor: pointer;
+    background-color: var(--color-primary-two);
+    padding: 32px;
+    border-radius: 20px 20px 0px 0px;
+  }
+  input:checked + label {
+    background: var(--color-primary);
+    color: white;
+  }
+`;
+
+const appearFromLeft = keyframes`
+from {
+    opacity: 0;
+    transform: translateX(-50px)
+}
+
+to {
+    opacity: 1;
+    transform: translateX(0px)
+}
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 1rem;
+  animation: ${appearFromLeft} 1s;
+
+  form {
+    margin: 60px 0;
+    width: 270px;
+    text-align: center;
+    line-height: 2rem;
+
+    @media (min-width: 768px) {
+      width: 350px;
+    }
+
+    h1 {
+      margin-bottom: 32px;
+      font-family: var(--font-text);
+      font-weight: 700;
+      font-size: 2rem;
+      text-align: left;
+    }
+
+    > div {
+      margin-top: 16px;
+      font-family: var(--font-text);
+    }
+
+    p {
+      margin-top: 8px;
+      > a {
+        font-weight: 700;
+        color: var(--color-primary-two);
+      }
+    }
+  }
+`;
