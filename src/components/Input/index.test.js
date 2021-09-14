ee18d1d50d14752105d42ad56372634cb74a17b5
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-14 15:48:22 -0300

    feat: component of input's test created

--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -1,1 +1,21 @@
+import React from "react";
+import ActionRegister from "./index";
 
+import { render, waitFor, fireEvent } from "@testing-library/react";
+
+describe("Test for Action register", () => {
+  it("should add new input when form has been submitted", async () => {
+    //renderizar o componente
+    const { getByTestId } = render(<ActionRegister />);
+
+    //buscar o input
+    const fieldActionNode = await waitFor(() => getByTestId("input-field"));
+
+    // console.log(fieldActionNode);
+    const newTesting = "testing";
+    fireEvent.change(fieldActionNode, { target: { value: newTesting } });
+
+    //digitar no input
+    expect(fieldActionNode.value).toEqual(newTesting);
+  });
+});
