ed5b6af5940596aca769d7a3b0f241a89a1fe034
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-15 10:01:04 -0300

    feat:input test added

--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ActionRegister from "./index";
+import Input from "./index";
 
 import { render, waitFor, fireEvent } from "@testing-library/react";
 
-describe("Test for Action register", () => {
+describe("Testing Input", () => {
   it("should add new input when form has been submitted", async () => {
     //renderizar o componente
-    const { getByTestId } = render(<ActionRegister />);
+    const { getByTestId } = render(<Input register={() => {}} />);
 
     //buscar o input
     const fieldActionNode = await waitFor(() => getByTestId("input-field"));
