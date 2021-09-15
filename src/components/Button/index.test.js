94dea54b4b7e5ffb09a9af52598fc115b19fbfa0
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-15 10:29:45 -0300

    fix:refactoring code button test

--- src/components/Button/index.test.js
+++ src/components/Button/index.test.js
@@ -5,11 +5,7 @@ import { render, waitFor, fireEvent } from "@testing-library/react";
 
 describe("Test for Action register", () => {
   it("should check if the button runs right", async () => {
-    //renderizar o componente
     const { getByTestId } = render(<Button />);
-
-    //buscar o botao
-
     const btnNode = await waitFor(() => getByTestId("form-btn"));
     fireEvent.click(btnNode); //clicar no botao
   });
