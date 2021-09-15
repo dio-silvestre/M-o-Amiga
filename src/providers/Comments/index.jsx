587fcada5d6c0b19e9aed794847709a0ee7a59c0
José Flávio <jotaftm@gmail.com>
2021-09-15 20:08:09 -0300

    feat: provider comments.

---
+++ src/providers/Comments/index.jsx
@@ -0,0 +1,86 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { api } from "../../services/api";
+import jwtDecode from "jwt-decode";
+
+const CommentsContext = createContext();
+
+export const CommentsProvider = ({ children }) => {
+
+    const localToken = localStorage.getItem("authToken") || "";
+    const decodedToken = localToken === "" ? "" : jwtDecode(localToken);
+    const userID = decodedToken.sub;
+
+    const [comments, setComments] = useState([]);
+
+    const fetchComments = () => {
+        api
+        .get(`/comments`, {
+            headers: {
+              Authorization: `Bearer ${localToken}`,
+            },
+          })
+        .then((response) =>{
+            setComments(response.data);
+        })
+        .catch((error) => console.error(error));
+    };
+
+    useEffect(() => {
+        fetchComments();
+        // setInterval(fetchComments, 2000);
+    }, []);
+
+    const addComment = (comment, actionId) => {
+        api
+        .post(`/comments`, {
+            comment: comment,
+            actionID: actionId,
+            userId: Number(userID)
+        }, {
+            headers: {
+              Authorization: `Bearer ${localToken}`,
+            },
+          })
+        .then((response) =>{
+            setComments([...comments, response.data]);
+        })
+        .catch((error) => console.error(error));
+    };
+
+    const editComment = (newComment, commentId) => {
+        api
+        .patch(`/comments/${commentId}`, {
+            comment: newComment,
+            userId: Number(userID)
+        }, {
+            headers: {
+              Authorization: `Bearer ${localToken}`,
+            },
+          })
+        .then((response) =>{
+            setComments([...comments, response.data]);
+        })
+        .catch((error) => console.error(error));
+    };
+
+    const deleteComment = (commentId) => {
+        api
+        .delete(`/comments/${commentId}`, {
+            headers: {
+              Authorization: `Bearer ${localToken}`,
+            },
+          })
+        .then((_) =>{
+            fetchComments();
+        })
+        .catch((error) => console.error(error));
+    };
+
+    return (
+        <CommentsContext.Provider value={{comments, addComment, editComment, deleteComment}}>
+            {children}
+        </CommentsContext.Provider>
+    );
+};
+
+export const useComments = () => useContext(CommentsContext);
