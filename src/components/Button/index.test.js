e6d5ceede0f405caf3c86077714cdb34a7054716
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-15 10:30:43 -0300

    fix: correcting descbri of button's test

--- src/components/Button/index.test.js
+++ src/components/Button/index.test.js
@@ -3,10 +3,10 @@ import Button from "./index";
 
 import { render, waitFor, fireEvent } from "@testing-library/react";
 
-describe("Test for Action register", () => {
+describe("Testing button", () => {
   it("should check if the button runs right", async () => {
     const { getByTestId } = render(<Button />);
     const btnNode = await waitFor(() => getByTestId("form-btn"));
-    fireEvent.click(btnNode); //clicar no botao
+    fireEvent.click(btnNode);
   });
 });
