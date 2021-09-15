59b530b206725377c5f26d18ae2eeaff521177d5
Raphael Marum de Freitas <raphael.marum@unesp.br>
2021-09-15 10:28:53 -0300

    fix:refactoring code

--- src/components/Input/index.test.js
+++ src/components/Input/index.test.js
@@ -5,17 +5,10 @@ import { render, waitFor, fireEvent } from "@testing-library/react";
 
 describe("Testing Input", () => {
   it("should add new input when form has been submitted", async () => {
-    //renderizar o componente
     const { getByTestId } = render(<Input register={() => {}} />);
-
-    //buscar o input
     const fieldActionNode = await waitFor(() => getByTestId("input-field"));
-
-    // console.log(fieldActionNode);
     const newTesting = "testing";
     fireEvent.change(fieldActionNode, { target: { value: newTesting } });
-
-    //digitar no input
     expect(fieldActionNode.value).toEqual(newTesting);
   });
 });
